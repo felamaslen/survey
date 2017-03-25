16ed62394857f13987ae613ffe1c0db960849db0
Fela Maslen <felamaslen@gmail.com>
2017-03-25 17:02:11 +0000

    Optimise react for production

--- webpack/webpack.staging.config.js
+++ webpack/webpack.staging.config.js
@@ -8,6 +8,11 @@ module.exports = {
     './src/js/index.jsx'
   ],
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin()
   ],
   output: {
