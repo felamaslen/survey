93f9a3254deed8a09d0527082dccb052eee97ece
Fela Maslen <felamaslen@gmail.com>
2017-03-25 12:41:09 +0000

    Moved header to its own component

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PureControllerView from './PureControllerView';
+
+export class Header extends PureControllerView {
+  render() {
+    return (
+      <div id="header">
+        <h1>Survey Form</h1>
+      </div>
+    );
+  }
+}
