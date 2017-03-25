d774905f5cbd4def9ca78155acef85bacb2d195b
Fela Maslen <felamaslen@gmail.com>
2017-03-25 16:03:13 +0000

    Button to reset form and make a new response

--- src/js/reduction.js
+++ src/js/reduction.js
@@ -1,32 +1,15 @@
 import { Record, List, fromJS } from 'immutable';
 
 import {
-  FORM_TITLES
+  formDefaultValues
 } from './config';
-
-const getCurrentDateTime = () => {
-  const isoString = new Date().toISOString();
-  return isoString.substring(0, isoString.lastIndexOf(':'));
-}
 
 // the state of the app (reduction) is stored as an immutable object,
 // and returned (modified) by reducers
 export default new Record({
   appState: fromJS({
     formStep: 0, // goes to 1 when the second part is displayed
-    formValues: [
-      // each object in this list corresponds to a separate form section
-      {
-        title: FORM_TITLES[0],
-        name: '',
-        dob: ''
-      },
-      {
-        location: '',
-        datetime: getCurrentDateTime(),
-        feedback: ''
-      }
-    ],
+    formValues: formDefaultValues(),
     formLoading: false,
     formSubmitted: false
   }),
